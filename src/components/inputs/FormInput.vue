7495bc0477f764445e631570ee61b7564d8753e5
Ivan <prostoyachelovek@gmail.com>
2024-02-03 20:07:18 +0300

    input styles

--- src/components/inputs/FormInput.vue
+++ src/components/inputs/FormInput.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { vMaska } from "maska"
 
+import { ref } from "vue";
+
 
 defineProps<{
     placheholder: string,
 }>()
+
+const inputValue = ref<string>()
 </script>
 <template>
     <div class="input pt-5">
@@ -12,10 +16,17 @@ defineProps<{
             <input 
                 type="text"
                 class="bg-stone-800 py-3.5 px-2 text-lg rounded w-full z-10"
+
                 v-maska
                 data-maska="##.##.####"
+                v-model="inputValue"
             >
-            <div class="input__placeholder absolute left-2 top-1/2 -translate-y-1/2 z-0 select-none transition-all duration-200 ease-out opacity-80">
+            <div 
+                class="input__placeholder absolute left-2 top-1/2 -translate-y-1/2 z-0 select-none transition-all duration-200 ease-out opacity-80"
+                :class="{
+                    'input__placeholder_active': inputValue
+                }"
+            >
                 {{ placheholder }}
             </div>
         </div>
@@ -24,9 +35,10 @@ defineProps<{
 <style scoped lang="sass">
 input:focus + .input__placeholder, .input__placeholder_active
     color: currentColor
-    top: 0
+    top: -5px
     transform: translateY(-100%)
     opacity: 1
+    font-size: 18px
 
 input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, input:-webkit-autofill:active
     -webkit-background-clip: text
